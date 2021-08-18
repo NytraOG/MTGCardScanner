ec7e0f0b8ae94526a90877ebe648fe4b4b4efb4a
Christian Nytra <christian.nytra@co-orga.d>
2021-08-18 23:32:46 +0200

    Init

---
+++ OCRLibrary.Tests/ScanServiceTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OCRLibrary.Tests
+{
+    [TestClass]
+    public class ScanServiceTests
+    {
+        private ScanService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            service = new ScanService();
+        }
+
+        [TestMethod]
+        public void METHOD_SCENARIO_RESULT()
+        {
+            // Arrange
+            
+
+            // Act
+            service.MachScan();
+
+            // Assert
+        }
+    }
+}
---
+++ OCRLibrary/ScanService.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Tesseract;
+
+namespace OCRLibrary
+{
+    public class ScanService
+    {
+        private List<string> texte;
+
+        public void MachScan()
+        {
+            texte = new List<string>();
+
+            GibCardText("name.tiff");
+            GibCardText("text.tiff");
+        }
+
+        private void GibCardText(string fileName)
+        {
+            var imagepath = Directory.GetCurrentDirectory() + $@"\Assets\{fileName}";
+
+            using var engine = new TesseractEngine(@"./Tessdata", "eng", EngineMode.Default);
+            using var image  = Pix.LoadFromFile(imagepath);
+            using var page = engine.Process(image);
+
+            texte.Add(page.GetText());
+        }
+    }
+}
